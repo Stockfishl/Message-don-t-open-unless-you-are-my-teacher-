2688bb53f86c63921c0d4734c4ebc098038c7ce6
Stockfishl <s.egor9463@gmail.com>
2023-12-02 18:55:49 +0200

    Init

---
+++ main.py
@@ -0,0 +1,2 @@
+print('Helloooooo everyone!Stockfish is online!Oh sorry,I meant Stetsiuk Egor of course,Stockfish is just a nickname.'
+      'I dont really know what to say here,uhm,have a great day and breakfast!')
